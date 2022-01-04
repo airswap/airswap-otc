ffe39573dbb9fc029bbdaa56c1b1fb331ec9c778
dmosites <mosites@gmail.com>
2022-01-04 19:52:41 +0000

    Deploying to gh-pages from @ airswap/airswap-otc@23de4ab8da2855abe620d33183f3ac33df4aec7b 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bbc58ba710e9b4d74fdd89cc408fb7aa.js"
+  "/precache-manifest.baf746a582b0775f5d7399686a3fb945.js"
 );
 
 workbox.clientsClaim();
